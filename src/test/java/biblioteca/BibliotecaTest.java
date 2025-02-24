0e231093986ed6230ebc280e163c2df01a571b6c
Luis García Sánchez <luis.garciasanchez@unir.net>
2025-02-24 14:56:11 +0100

    UPDATE: Actualizados Main y Biblioteca

---
+++ src/test/java/biblioteca/BibliotecaTest.java
@@ -0,0 +1,37 @@
+package biblioteca;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BibliotecaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testAgregarLibro() {
+    }
+
+    @Test
+    void testEliminarLibro() {
+    }
+
+    @Test
+    void testGetLibros() {
+    }
+
+    @Test
+    void testEncuentraLibroPorTitulo() {
+    }
+
+    @Test
+    void testEncuentaLibroPorAutor() {
+    }
+
+    @Test
+    void testEncuentraLibrosPorAutor() {
+    }
+}
